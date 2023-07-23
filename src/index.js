bb4f3cf037541a6b0a68d093027ead6373f58be6
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-07-23 17:14:31 -0300

    primeiras linhas de código, addEventListener e getElementById inseridos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
+id="cardNumber"; // caixa do número do cartão
+id="pagCardNumber"; //botão de validar o número
+
+
+const confirmButton = document.getElementById("pagCardNumber"); //liguei o id do botão de pagamento à const.
+
+confirmButton.addEventListener("click",function(event) { //addEventListener "ouve" a ação, no caso o click
+event.preventDefault() //preventDeFault() não deixa o click abrir oura página que seria o esperado
+
+
+
+
+
+
+
+
+
+});
