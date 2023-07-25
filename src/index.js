77a3afc9c36cd0df000f94c8a0851844416bfb5a
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-07-25 17:59:48 -0300

    códigos para substituir espaços por strings vazias e validar cartão

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 import validator from './validator.js';
 
+
+const cardNumber = document.getElementById("numCartao"); //caixa que recebe os números do cartão
+const pagCardNumber = document.getElementById("enviar"); // botão de pagamento
+const validCard = document.getElementById("cartaoValido"); // mensagem de validação
+const emptySpace = document.getElementById("espacoVazio");// avisar espaço vazio
+
+
+//encontrei uma outra forma de validar o cartão
+function isValid(cardNumber) {
+
+    cardNumber = cardNumber.replace(/\s/g, '') //remove todos os espaços em branco e substitui por string vazia
+
+    if (!/^\d+$/.test(cardNumber)) { //lá no começo dos parênteses tem um ! que denota negação de toda a ação seguinte
+        return false;                  //verifica se o cartão possui números de [0-9], se não houver então retorna false
+      }
+
+      if (cardNumber.length === 16) { //se tiver 16 números retorna true portanto é válido
+        return true
+      }
+    };
+
+
+pagCardNumber.addEventListener("click", function() { //chamei a const pagCardNumber que é o botão de validar
+
+const cardNumber = /*validator.*/ isValid(cardNumber); //a const chama o algoritmo de luhn em cima do número do cartão
+if (cardNumber === true) {      //a condição para a função ser verdadeira é que o número do cartão seja válido pelo algoritmo avisa dentro do
+validCard.innerHTML = /*value +*/ "Seu CARTÃO É VÁLIDO!" //innerHTML é onde vai aparecer se é válido ou não através do id=cartaoValido
+} else { //ou, caso a sequência numérica não seja compatível com o algoritmo a mensagem de inválido é disparada no html tb
+    validCard.innerHTML = /*value*/ + " Seu CARTÃO É INVÁLIDO! Certifique-se de ter digitado os números corretamente."
+}
+})
+
+//falta o algoritmo de luhn e o código que oculta os caracteres
+
+
 console.log(validator);
-
-
-
-id="cardNumber"; // caixa do número do cartão
-id="pagCardNumber"; //botão de validar o número
-
-
-const confirmButton = document.getElementById("pagCardNumber"); //liguei o id do botão de pagamento à const.
-
-confirmButton.addEventListener("click",function(event) { //addEventListener "ouve" a ação, no caso o click
-event.preventDefault() //preventDeFault() não deixa o click abrir oura página que seria o esperado
-
-
-
-
-
-
-
-
-
-});
