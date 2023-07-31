c4f7b95875cdd553f7e3ffe9c9ab420baf9d6532
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-07-31 13:49:48 -0300

    resolvi o validador, ufa

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import validator from './validator.js';
 
-
-const cardNumber = document.getElementById("numCartao"); //caixa que recebe os números do cartão
-const pagCardNumber = document.getElementById("enviar"); // botão de pagamento
-const validCard = document.getElementById("cartaoValido"); // mensagem de validação
-const emptySpace = document.getElementById("espacoVazio");// avisar espaço vazio
-
-
-//encontrei uma outra forma de validar o cartão
-function isValid(cardNumber) {
-
-    cardNumber = cardNumber.replace(/\s/g, '') //remove todos os espaços em branco e substitui por string vazia
-
-    if (!/^\d+$/.test(cardNumber)) { //lá no começo dos parênteses tem um ! que denota negação de toda a ação seguinte
-        return false;                  //verifica se o cartão possui números de [0-9], se não houver então retorna false
-      }
-
-      if (cardNumber.length === 16) { //se tiver 16 números retorna true portanto é válido
-        return true
-      }
-    };
+//manipulando o DOM
+const botaoEnviar = document.getElementById("enviar");
+const cardNumber = document.getElementById("numCartao");
+const msgValidacao = document.getElementById("validacao");
 
 
-pagCardNumber.addEventListener("click", function() { //chamei a const pagCardNumber que é o botão de validar
 
-const cardNumber = /*validator.*/ isValid(cardNumber); //a const chama o algoritmo de luhn em cima do número do cartão
-if (cardNumber === true) {      //a condição para a função ser verdadeira é que o número do cartão seja válido pelo algoritmo avisa dentro do
-validCard.innerHTML = /*value +*/ "Seu CARTÃO É VÁLIDO!" //innerHTML é onde vai aparecer se é válido ou não através do id=cartaoValido
-} else { //ou, caso a sequência numérica não seja compatível com o algoritmo a mensagem de inválido é disparada no html tb
-    validCard.innerHTML = /*value*/ + " Seu CARTÃO É INVÁLIDO! Certifique-se de ter digitado os números corretamente."
+
+botaoEnviar.addEventListener("click",function() { 
+ const numCard = cardNumber.value; //importa do id do html os números do cartão  
+  if (/^\d{13,19}$/.test(numCard)) { //condicional para validar é não ter espaços e ter entre 13 e 16 (quando eu tiro o ! acusa um erro)
+  msgValidacao.innerHTML = "Cartão Válido!";
+
+  }else{
+
+  msgValidacao.innerHTML = "Cartão Inválido!";
 }
-})
 
-//falta o algoritmo de luhn e o código que oculta os caracteres
+//const validacaoFinal = validator.isValid(numCard);
 
+const digitCamuflados = validator.maskify(numCard); //tá funcionando, não mexer!
+console.log(digitCamuflados); //
+});
+
+
+ 
 
 console.log(validator);
+
+
+//fazer a lógica para ver se o cartão é válido
+// if (!/^\d{13,19}$/.test(numCard)) { //parâmetro para verificar se o numCard é válido, o que é verdadeiro já está aqui neste parâmetro
+//   submit.textContent = " Digite os números."; //
+//   submit.classList.add("error"); // pesquisar classlist
+//   return;
+// }
