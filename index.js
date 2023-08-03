8d4237ebe8143d2ba31ca4358719d340a2080dea
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-08-03 18:21:42 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,30 @@
+import validator from './validator.js';
+
+//manipulando o DOM
+const cardNumber = document.getElementById("numCartao");
+const botaoEnviar = document.getElementById("enviar");
+const msgValidacao = document.getElementById("validacao");
+
+
+
+
+botaoEnviar.addEventListener("click",function() { 
+  const numCard = cardNumber.value; //importa do id do html os números do cartão  
+  if (/^\d$/.test && validator.isValid(numCard)) { //condicional não ter espaços e ter entre 13 e 16 
+    msgValidacao.innerHTML = "Cartão Válido!";
+  }else{
+    msgValidacao.innerHTML = "Cartão Inválido!";
+  }
+
+  const validacaoFinal = validator.isValid(numCard);
+  document.getElementById("true-false-boolean").innerText = validacaoFinal;
+  console.log(validator);
+
+  const digitCamuflados = validator.maskify(numCard); //tá funcionando, não mexer!
+  document.getElementById("numeros-mascarados").innerHTML = digitCamuflados;
+  console.log(digitCamuflados); 
+});
+
+
+
+
---
+++ validator.js
@@ -0,0 +1,55 @@
+const validator = { 
+  
+  //começa validação pelo algoritmo de Luhn
+  isValid:function(numCard) {//a função usa como parâmetro o numCard que é o número do cartão
+    //colocar os números dentro de uma array
+    const cardArrays = []; //array vazia para receber as strings do cartão
+    let soma = 0; //calcula a soma dos dígitos do cartão
+
+    //passa por cada número do cartão converte em número inteiro
+    for (let index = 0; index < numCard.length; index++) {
+      const cadaNumero = numCard[index];
+      cardArrays.push(parseInt(cadaNumero));
+    }
+
+
+    //a partir do penúltimo dígito, multiplica por 2 os números pares
+    for (let index = cardArrays.length - 2; index >= 0; index -= 2) {
+      const digitoAtual = cardArrays[index] * 2;
+      if (digitoAtual > 9) { //se o resultado for maior que 9 soma o produto para virar 1 dígito
+        cardArrays[index] = digitoAtual % 10 + 1; 
+      } else {
+        cardArrays[index] = digitoAtual; // ou não faz nada
+      }
+    }
+  
+    for (let index = 0; index < cardArrays.length; index++) {//soma todos os dígitos
+      soma = soma + cardArrays[index];
+    }
+    
+    if (soma % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+        
+  },
+
+  maskify:function(numCard){  //maskify deu certo, não mexer!
+  
+    let digitMascarados = ""; //cria uma variável incialmente vazia que armazenará o número do cartão
+    if(numCard.length>4){ //verifica se tem mais de 4 dígitos
+      const ultimosDigitos = numCard.substr(-4); //subtrai os 4 últimos 
+      const digitosOcultos = "#".repeat(numCard.length - 4); //coloca # nos demais
+      digitMascarados = digitosOcultos + ultimosDigitos;      
+    } else {
+      digitMascarados = numCard;
+    }
+    return digitMascarados
+  
+  },
+
+};
+   
+export default validator; 
+
