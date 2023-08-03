c3d981ea01667313013dbe02ee25ec0058ff264b
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-08-03 13:43:16 -0300

    tudo ok

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const msgValidacao = document.getElementById("validacao");
 
 botaoEnviar.addEventListener("click",function() { 
   const numCard = cardNumber.value; //importa do id do html os números do cartão  
-  if (/^\d{13,19}$/.test(numCard)) { //condicional não ter espaços e ter entre 13 e 16 
+  if (/^\d$/.test && validator.isValid(numCard)) { //condicional não ter espaços e ter entre 13 e 16 
     msgValidacao.innerHTML = "Cartão Válido!";
   }else{
     msgValidacao.innerHTML = "Cartão Inválido!";
